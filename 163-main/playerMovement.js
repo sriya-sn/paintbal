040159383484f928df24405ed6cdba20f65816d6
sriya-sn <81259869+sriya-sn@users.noreply.github.com>
2024-06-23 12:38:31 -0400

    Add files via upload

---
+++ 163-main/playerMovement.js
@@ -0,0 +1,18 @@
+AFRAME.registerComponent("player-movement", {
+  init: function () {
+    this.walk();
+  },
+  walk: function () {
+    window.addEventListener("keydown", (e) => {
+      if (
+        e.key === "ArrowUp" ||
+        e.key === "ArrowRight" ||
+        e.key === "ArrowLeft" ||
+        e.key === "ArrowDown"
+      ) {
+   //add the sound for movement
+      }
+    });
+  },
+});
+
